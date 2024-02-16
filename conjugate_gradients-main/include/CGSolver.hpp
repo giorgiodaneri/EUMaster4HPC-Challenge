f537c3272a3d280740fdcaa07b1379b98988e043
giorgiodaneri <giorgio1.daneri@gmail.com>
2024-02-16 11:39:31 +0100

    Started implementation of OpenACC, slightly changed test folder structure and relative paths

--- conjugate_gradients-main/include/CGSolver.hpp
+++ conjugate_gradients-main/include/CGSolver.hpp
@@ -10,13 +10,12 @@
 class CGSolver
 {
 public:
-    CGSolver(double *A, double *b, double *x, size_t size, int maxIterations, double tolerance) : 
-    A_(A), 
-    b_(b), 
-    x_(x), 
-    size_(size), 
-    max_iter_(maxIterations), 
-    tol_(tolerance) {}
+    CGSolver(double *A, double *b, double *x, size_t size, int maxIterations, double tolerance) : A_(A),
+                                                                                                  b_(b),
+                                                                                                  x_(x),
+                                                                                                  size_(size),
+                                                                                                  max_iter_(maxIterations),
+                                                                                                  tol_(tolerance) {}
 
     ~CGSolver() = default;
 
@@ -24,22 +23,24 @@ public:
     // void setA(double *A) {A_ = A;}
     // void setB(double *b) {b_ = b;}
     // void setX(double *x) {x_ = x;}
-    void setTolerance(double tolerance) {tol_ = tolerance;}
-    void setMaxIter(int maxIterations) {max_iter_ = maxIterations;}
-    double *getA() {return A_;}
-    double *getB() {return b_;}
-    double *getX() {return x_;}
-    size_t getSize() {return size_;}
-    int getMaxIter() {return max_iter_;}
-    double getRelErr() {return tol_;}
+    void setTolerance(double tolerance) { tol_ = tolerance; }
+    void setMaxIter(int maxIterations) { max_iter_ = maxIterations; }
+    double *getA() { return A_; }
+    double *getB() { return b_; }
+    double *getX() { return x_; }
+    size_t getSize() { return size_; }
+    int getMaxIter() { return max_iter_; }
+    double getRelErr() { return tol_; }
     void solve();
-    void solveOmp();
-    
+    void solveOpenACC();
+
 protected:
     double dot(const double *x, const double *y, size_t size);
     void axpby(double alpha, const double *x, double beta, double *y, size_t size);
     void precA(const double *A, const double *x, double *Ax, size_t size);
-
+    double dot_acc(const double *x, const double *y, size_t size);
+    void axpby_acc(double alpha, const double *x, double beta, double *y, size_t size);
+    void precA_acc(const double *A, const double *x, double *Ax, size_t size);
 
 private:
     // system matrix A, square and spd
