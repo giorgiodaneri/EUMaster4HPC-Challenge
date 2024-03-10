db5a73d8c591e45970afbbf1c3420e427f407933
Miglia <100707335+Miglia12@users.noreply.github.com>
2024-03-10 13:08:06 +0100

    Update batch_MPI.sh

--- conjugate_gradients-main/batches/batch_MPI.sh
+++ conjugate_gradients-main/batches/batch_MPI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --cpus-per-task=1                                            # Set the number of CORES per task
+#SBATCH --cpus-per-task=16                                            # Set the number of CORES per task
 #SBATCH --qos=default                                                # SLURM qos
 #SBATCH --nodes=6                                                   # number of nodes
 #SBATCH --ntasks=30                                                   # number of tasks
@@ -13,23 +13,15 @@ BUILD_DIR="$HOME/EUMaster4HPC-Challenge/conjugate_gradients-main/build/test/test
 MATRIX_PATH="$HOME/EUMaster4HPC-Challenge/conjugate_gradients-main/io/matrix.bin"
 RHS_PATH="$HOME/EUMaster4HPC-Challenge/conjugate_gradients-main/io/rhs.bin"
 SOLUTION_PATH="$HOME/EUMaster4HPC-Challenge/conjugate_gradients-main/io/sol.bin"
-SAMPLE_PY_PATH="$HOME/EUMaster4HPC-Challenge/conjugate_gradients-main/sample.py"
-
-# Number of samples to take
-NUM_SAMPLES=10
 
 module load CMake OpenMPI 
 
-rm -r build
-mkdir build
-cd build
+mkdir -p ../build
+cd ../build
 
 cmake .. -DBUILD_MPI=ON
 make
 
-chmod +rx "$SAMPLE_PY_PATH"
-
 echo "--------------------------------------------------------------------------------"
 
-srun "$SAMPLE_PY_PATH" $NUM_SAMPLES "$BUILD_DIR"/mainMPI_OpenMP "$MATRIX_PATH" "$RHS_PATH" "$SOLUTION_PATH"
-
+srun "$BUILD_DIR"/mainMPI_OpenMP "$MATRIX_PATH" "$RHS_PATH" "$SOLUTION_PATH"
